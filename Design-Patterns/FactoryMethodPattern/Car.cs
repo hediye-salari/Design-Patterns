1d93b2ea31d5b44dff51548433f890ba8e089243
hp <Salari.hediye@gmail.com>
2025-03-22 15:19:24 +0430

    impliment Factory Method Pattern

---
+++ Design-Patterns/FactoryMethodPattern/Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Design_Patterns.FactoryMethodPattern
+{
+    public class Car : IVehicle
+    {
+        public void Drive()
+        {
+            Console.WriteLine("Driving a car!");
+        }
+    }
+
+}
