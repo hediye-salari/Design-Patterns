d39565aef9362136c2267eba739aec3690a303f4
hp <Salari.hediye@gmail.com>
2025-03-22 21:56:55 +0430

    Concrete Products : Different types of notifications

---
+++ Design-Patterns/FactoryMethodPattern/NotificationSystem/EmailNotification .cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Design_Patterns.FactoryMethodPattern.NotificationSystem
+{
+    public class EmailNotification : INotification
+    {
+        public void Send(string message)
+        {
+            Console.WriteLine($"Sending Email: {message}");
+        }
+    }
+    public class SmsNotification : INotification
+    {
+        public void Send(string message)
+        {
+            Console.WriteLine($"Sending SMS: {message}");
+        }
+    }
+
+    public class PushNotification : INotification
+    {
+        public void Send(string message)
+        {
+            Console.WriteLine($"Sending Push Notification: {message}");
+        }
+    }
+
+
+
+
+
+
+}
