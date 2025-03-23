0dad563698ba971bb8ac20d59e18ce1e0c167722
hp <Salari.hediye@gmail.com>
2025-03-23 20:34:38 +0430

    Add MacOSCheckbox Class

---
+++ Design-Patterns/AbstractFactoryPattern/MacOSCheckbox.cs
@@ -0,0 +1,18 @@
+﻿using Design_Patterns.AbstractFactoryPattern.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Design_Patterns.AbstractFactoryPattern
+{
+    public class MacOSCheckbox : ICheckbox
+    {
+        public void Render()
+        {
+            Console.WriteLine("Rendering a macOS Checkbox.");
+        }
+    }
+
+}
