e3723ffab02e4c8f804ee41b8abb7274b57141a4
hp <Salari.hediye@gmail.com>
2025-03-23 15:26:31 +0430

    Concrete Prototype: Character Class

---
+++ Design-Patterns/PrototypePattern/Character.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Design_Patterns.PrototypePattern
+{
+    public class Character : ICloneable<Character>
+    {
+
+        public string Name { get; set; }
+        public int Level { get; set; }
+        public int Power { get; set; }
+
+        public Character(string name, int level, int power)
+        {
+            Name = name;
+            Level = level;
+            Power = power;
+        }
+
+        public Character Clone()
+        {
+           return new Character(Name, Level, Power);
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"Character: {Name}, Level: {Level}, Power: {Power}");
+        }
+    }
+}
