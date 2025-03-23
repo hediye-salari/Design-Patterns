763311265c1bd5869deae5396a97d330e64b07f1
hp <Salari.hediye@gmail.com>
2025-03-23 20:11:49 +0430

    Create Client Code

---
+++ Design-Patterns/AbstractFactoryPattern/Application.cs
@@ -0,0 +1,27 @@
+﻿using Design_Patterns.AbstractFactoryPattern.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Design_Patterns.AbstractFactoryPattern
+{
+    public  class Application
+    {
+        private readonly IButton _button;
+        private readonly ICheckbox _checkbox;
+        public Application(IGUIFactory factory)
+        {
+            _button= factory.CreateButton();
+            _checkbox= factory.CreateCheckbox();    
+
+        }
+
+        public void UIRender()
+        {
+            _button.Render();
+            _checkbox.Render();
+        }
+    }
+}
