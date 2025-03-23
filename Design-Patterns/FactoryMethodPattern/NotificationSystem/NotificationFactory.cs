87d47710004198a6db79715e87070d64cf043b4c
hp <Salari.hediye@gmail.com>
2025-03-23 12:28:05 +0430

    Creator: Abstract class that defines the factory method

---
+++ Design-Patterns/FactoryMethodPattern/NotificationSystem/NotificationFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Design_Patterns.FactoryMethodPattern.NotificationSystem
+{
+    public abstract class NotificationFactory
+    {
+        public abstract INotification CreateNotification();
+
+        public void SendNotification(string message)
+        {
+            var notification = CreateNotification();
+            Console.WriteLine("Preparing to send notification...");
+            notification.Send(message);
+        }
+    }
+}
+
