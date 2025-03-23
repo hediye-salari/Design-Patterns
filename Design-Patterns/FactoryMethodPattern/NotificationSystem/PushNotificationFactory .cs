6bdc848ac16f70b21ff6dfbce332e093692be006
hp <Salari.hediye@gmail.com>
2025-03-23 12:33:47 +0430

    Concrete Creators: Factories for specific notification types

---
+++ Design-Patterns/FactoryMethodPattern/NotificationSystem/PushNotificationFactory .cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Design_Patterns.FactoryMethodPattern.NotificationSystem
+{
+    public class PushNotificationFactory : NotificationFactory
+    {
+        public override INotification CreateNotification()
+        {
+           return new PushNotification();   
+        }
+    }
+}
---
+++ Design-Patterns/FactoryMethodPattern/NotificationSystem/SmsNotificationFactory .cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Design_Patterns.FactoryMethodPattern.NotificationSystem
+{
+    public class SmsNotificationFactory : NotificationFactory
+    {
+        public override INotification CreateNotification()
+        {
+           return new SmsNotification();
+        }
+    }
+}
