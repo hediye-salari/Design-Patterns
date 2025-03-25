a61da8261d900b53134f47654f7c7b9cf2237545
hp <Salari.hediye@gmail.com>
2025-03-25 13:15:39 +0430

    add initial class for Builder Pttern

---
+++ Design-Patterns/BuilderPttern/Computer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Design_Patterns.BuilderPttern
+{
+    public class Computer
+    {
+        public string CPU { get; set; }
+        public string RAM { get; set; }
+        public string Storage { get; set; }
+        public string GPU { get; set; }
+    }
+}
