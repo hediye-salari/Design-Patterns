9f15e133f571c40b85216fe89a7239325ada64af
hp <Salari.hediye@gmail.com>
2025-03-25 13:17:29 +0430

    add DisplaySpecs method in computer class

--- Design-Patterns/BuilderPttern/Computer.cs
+++ Design-Patterns/BuilderPttern/Computer.cs
@@ -8,9 +8,17 @@ namespace Design_Patterns.BuilderPttern
 {
     public class Computer
     {
-        public string CPU { get; set; }
-        public string RAM { get; set; }
-        public string Storage { get; set; }
-        public string GPU { get; set; }
+        public required string CPU { get; set; }
+        public required string RAM { get; set; }
+        public required string Storage { get; set; }
+        public required string GPU { get; set; }
+
+        public void DisplaySpecs()
+        {
+            Console.WriteLine($"CPU: {CPU}");
+            Console.WriteLine($"RAM: {RAM}");
+            Console.WriteLine($"Storage: {Storage}");
+            Console.WriteLine($"GPU: {(GPU ?? "Integrated")}");
+        }
     }
 }
